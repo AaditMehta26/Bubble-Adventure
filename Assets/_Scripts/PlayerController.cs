1f6f0a7af57419de5a98da656b088b8d5633722d
Devansh Rawat <bikkarawat28@gmail.com>
2025-01-25 11:13:45 +0530

    Player indicator

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,29 +1,30 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
-
-
 public class PlayerController : MonoBehaviour
 {
     
-    #region [Serialised]Variables
-        [SerializeField]float gravity = -9.81f;
+    #region [Serialised]fields
+        //references
         [SerializeField]private ParticleSystem deathParticles;
+        [SerializeField] private Transform indicator;
+        //variables
+        [SerializeField]float gravity = -9.81f;
         [SerializeField] private float maxVelocity = 0.5f;
         [SerializeField]private float forceAmount;
         [Range(0f, 1f)]
         [SerializeField]private float slomoTime;
-    #endregion
-    #region Variables
 
+        [SerializeField] private float rotationSpeed;
+    #endregion
+    #region Fields
+    //references
     private InputSystem_Actions _playerActions;
     private Rigidbody2D _playerRb;
+    //variables
     private Vector3 _originalScale;
     private Vector2 _startPosition;
     private Vector2 _lastPosition;
     private Vector2 _dragVector;
-    private Vector3 _originalPosition;
     #endregion
 
     private void Awake()
@@ -39,11 +40,6 @@ public class PlayerController : MonoBehaviour
         _playerRb.gravityScale = gravity;
     }
 
-    private void Start()
-    {
-        _originalPosition = transform.localPosition;
-    }
-    
 
     private void OnEnable()
     {
@@ -52,27 +48,49 @@ public class PlayerController : MonoBehaviour
         
         _playerActions.Player.Press.canceled += OnLift;
     }
+    private void Update()
+    {
+        HandleIndicator();
+    }
     private void OnPress(InputAction.CallbackContext obj)
     {
         _startPosition = Mouse.current.position.ReadValue();
-        print("Pressed" + _startPosition);
         _playerActions.Player.Look.performed += OnDrag;
         Time.timeScale = slomoTime;
+        
+        indicator.gameObject.SetActive(true);
     }
     private void OnDrag(InputAction.CallbackContext obj)
     {
         _lastPosition = Mouse.current.position.ReadValue();
-        print("dragging" + _lastPosition);
+        _dragVector = _lastPosition - _startPosition;
     }
+
+    private void HandleIndicator()
+    {
+        if (_dragVector.sqrMagnitude > 0)
+        {
+            float angle = Mathf.Atan2(_dragVector.x, -_dragVector.y) * Mathf.Rad2Deg;
+            
+            var strength = _dragVector.magnitude/80;
+            var direction =- _dragVector.normalized;
+
+            indicator.transform.position =  Vector3.ClampMagnitude((Vector2)transform.position + direction * strength, maxVelocity);
+            indicator.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
     private void OnLift(InputAction.CallbackContext obj)
     {
         _lastPosition = Mouse.current.position.ReadValue();
-        print("Lifted" + _lastPosition);
         _playerActions.Player.Look.performed -= OnDrag;
         
         _dragVector = _lastPosition - _startPosition;
         Time.timeScale = 1f;
         ApplyForce(_dragVector);
+        
+        indicator.gameObject.SetActive(false);
+
     }
 
     private void ApplyForce(Vector2 dragVector)
